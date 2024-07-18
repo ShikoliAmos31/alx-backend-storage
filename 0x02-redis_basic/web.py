ac0270273eb6f392e5388ee856b92426c33eb450
shikoliamos31 <amosshikoli@gmail.com>
2024-07-18 04:49:38 +0300

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,52 +1,38 @@
 #!/usr/bin/env python3
-"""
-Web caching module
-"""
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
+from functools import wraps
 from typing import Callable
 
-# Initialize Redis client
-r = redis.Redis()
 
-def count_calls(method: Callable) -> Callable:
-    """Decorator to count the number of calls to a method"""
-    def wrapper(*args, **kwargs):
-        """Wrapper function to increment the count and call the original method"""
-        url = args[0]
-        r.incr(f"count:{url}")
-        return method(*args, **kwargs)
-    return wrapper
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
-def cache_result(method: Callable) -> Callable:
-    """Decorator to cache the result of a method with an expiration time"""
-    def wrapper(*args, **kwargs):
-        """Wrapper function to cache result and call the original method"""
-        url = args[0]
-        cached_result = r.get(url)
-        if cached_result:
-            return cached_result.decode('utf-8')
-        result = method(*args, **kwargs)
-        r.setex(url, 10, result)
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
-    return wrapper
+    return invoker
 
-@count_calls
-@cache_result
+
+@data_cacher
 def get_page(url: str) -> str:
-    """Get the HTML content of a URL and cache it with an expiration time"""
-    response = requests.get(url)
-    return response.text
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/3000/url/http://www.example.com"
-    print(get_page(url))
-    print(f"Access count: {r.get(f'count:{url}').decode('utf-8')}")
-    # Wait and try again to see if caching works
-    import time
-    time.sleep(5)
-    print(get_page(url))
-    print(f"Access count: {r.get(f'count:{url}').decode('utf-8')}")
-    time.sleep(6)
-    print(get_page(url))
-    print(f"Access count: {r.get(f'count:{url}').decode('utf-8')}")
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
